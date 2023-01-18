0a61679f5affa7772d76ac586a051b91a911817e
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-18 18:31:20 -0500

    Added FirstContainer on landing page
    Added FirstContainer on landing page

--- src/pages/landing/landingView.js
+++ src/pages/landing/landingView.js
@@ -3,10 +3,51 @@ import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_LANDING_PAGE_DATA_QUERY } from '../../bento/landingPageData';
+import img from '../../assets/landing/Landing_Intro_Pic.png'
 
 const LandingViewContainer = styled.div`
     width: 1440px;
     margin: 0 auto;
+`;
+
+const FirstContainer = styled.div`
+    display: flex;
+    height: 900px;
+    background: red;
+`;
+
+const IntroContainer = styled.div`
+    width: 720px;
+    background: yellow;
+`;
+
+const IntroTextContainer = styled.div`
+    height: 372px;
+    background: grey;
+`;
+
+const IntroPicContainer = styled.div`
+    width: 664px;
+    height: 434px;
+    background-image: url(${img});
+    background-repeat: no-repeat;
+    background-position: -34px;
+`;
+
+const ListContainer = styled.div`
+    width: 720px;
+    background: green;
+`;
+
+const StatsContainer = styled.div`
+    position: absolute;
+    height: 187px
+    width: 1119px;
+    background: #05555C;
+    border-radius: 0px 20px;
+    top: 713px;
+    margin-left: 161px;
+
 `;
 
 const LandingView = () => {
@@ -18,6 +59,18 @@ const LandingView = () => {
 
   return (
     <LandingViewContainer>
+      <FirstContainer>
+        <IntroContainer>
+          <IntroTextContainer>
+          </IntroTextContainer>
+          <IntroPicContainer>
+          </IntroPicContainer>
+        </IntroContainer>
+        <ListContainer>
+        </ListContainer>
+      </FirstContainer>
+      <StatsContainer>
+        </StatsContainer>
       <div style={{marginTop: "140px", height: "250px", fontSize: "100px"}}>Hello World!</div>
     </LandingViewContainer>
   )

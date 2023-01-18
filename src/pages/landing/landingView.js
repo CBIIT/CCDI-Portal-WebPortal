ce56f0816b7f7930b01636e38e017af432d9c975
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-18 16:56:53 -0500

    Added LandingViewContainer
    Added LandingViewContainer

--- src/pages/landing/landingView.js
+++ src/pages/landing/landingView.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_LANDING_PAGE_DATA_QUERY } from '../../bento/landingPageData';
+
+const LandingViewContainer = styled.div`
+    width: 1440px;
+    margin: 0 auto;
+`;
 
 const LandingView = () => {
   const { loading, error, data } = useQuery(GET_LANDING_PAGE_DATA_QUERY);
@@ -9,9 +15,11 @@ const LandingView = () => {
   useEffect(() => {
     console.log(data);
   },[data]);
-  
+
   return (
-    <div style={{marginTop: "140px", height: "250px", fontSize: "100px"}}>Hello World!</div>
+    <LandingViewContainer>
+      <div style={{marginTop: "140px", height: "250px", fontSize: "100px"}}>Hello World!</div>
+    </LandingViewContainer>
   )
 };
 

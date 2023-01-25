93c46bc784ddd239f3b192e92674bfdca64c9c6d
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-25 16:22:40 -0500

    Fixed the bug # in url
    Fixed the bug # in url

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,7 @@ import Layout from './Layout/LayoutView';
 // This is the place to check login ref to https://medium.com/@tomlarge/private-routes-with-react-router-dom-28e9f40c7146 for sample code
 
 const App = () => (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Layout} />
-      </Switch>
-    </BrowserRouter>
+    <Layout />
 );
 
 export default App;

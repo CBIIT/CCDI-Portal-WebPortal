af3bda126b1de1e732a49efef3026814d8065ea9
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-25 13:09:40 -0500

    changed HashRouter to BrowserRouter
    changed HashRouter to BrowserRouter

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { CssBaseline } from '@material-ui/core';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../Header/CustomizedHeader';
 import NavBar from '../NavBar/CustomizedNavBar';
 import Footer from '../Footer/FooterView';
@@ -10,33 +10,25 @@ import News from '../../pages/news/newsView';
 import Ccdi from '../../pages/ccdi/ccdiView';
 import Moonshot from '../../pages/moonshot/moonshotView';
 
-const ScrollToTop = () => {
-  window.scrollTo(0, 0);
-  return null;
-};
-
 const Layout = () => {
     return (
     <>
       <CssBaseline />
-      <HashRouter>
+      <Router>
         <>
           <Header />
           <NavBar />
-          <div>
-            <Route component={ScrollToTop} />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/news" component={News} />
-              <Route exact path="/ccdi" component={Ccdi} />
-              <Route exact path="/moonshot" component={Moonshot} />
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/news" component={News} />
+            <Route exact path="/ccdi" component={Ccdi} />
+            <Route exact path="/moonshot" component={Moonshot} />
+          </Switch>
           <Footer />
         </>
-      </HashRouter>
+      </Router>
     </>
     )
 }

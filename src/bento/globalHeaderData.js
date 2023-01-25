06020f7d649fc383bfebfa6d35bdb687a3462b6f
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-25 14:25:32 -0500

    Added searchbar and icon
    Added searchbar and icon

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -1,9 +1,12 @@
 import Logo from '../assets/header/Portal_Logo.svg';
+import searchbarIcon from '../assets/header/Search_Icon.svg';
 
 // globalHeaderLogo image 468x100
 // globalHeaderImage: image 2200x100
 export default {
   globalHeaderLogo: Logo,
   globalHeaderLogoLink: '/',
-  globalHeaderLogoAltText: 'Portal Logo'
+  globalHeaderLogoAltText: 'Portal Logo',
+  globalHeaderSearchIcon: searchbarIcon,
+  globalHeaderSearchIconAltText: 'search Icon',
 };
--- src/components/Header/CustomizedHeader.js
+++ src/components/Header/CustomizedHeader.js
@@ -2,31 +2,75 @@ import React from 'react';
 import styled from 'styled-components';
 import headerData from '../../bento/globalHeaderData';
 
-const LogoBanner = styled.div`
-  margin: 19px 0;
+const HeaderBanner = styled.div`
   width: 100%;
 `;
 
-const LogoContainer = styled.div`
+const HeaderContainer = styled.div`
+    display: flex;
     width: 1440px;
+    height: 90px;
     margin: 0 auto;
-    padding-left: 61px;
+    padding-left: 60px;
     img {
       width: 455px;
       height: 56px;
+    }
+
+    .logoContainer {
+      margin-top: 20px;
+    }
+
+    .searchBarContainer {
+      padding-right: 60px;
+      margin-left: auto;
+    }
+
+    .searchBar {
+      display: flex;
+      margin-top: 30px;
+      margin-left: auto;
+      width: 300px;
+      height: 36px;
+      border: 1px solid #004A8B;
+      border-radius: 18px;
+    }
+
+    .searchIcon {
+      margin-left: auto;
+      margin-right: 13px;
+      height: 20px;
+      width: 20px;
+      padding-top: 8px;
+    }
+
+    .searchIconImg {
+      height: 18px;
+      width: 18px;
+    }
+
+    .searchIconImg:hover {
+      cursor: pointer;
     }
 `;
 
 const Header = () => {
   return (
     <>
-      <LogoBanner role="banner">
-        <LogoContainer>
-          <a href={headerData.globalHeaderLogoLink}>
+      <HeaderBanner role="banner">
+        <HeaderContainer>
+          <a className='logoContainer' href={headerData.globalHeaderLogoLink}>
             <img src={headerData.globalHeaderLogo} alt={headerData.globalHeaderLogoAltText} />
           </a>
-        </LogoContainer>
-      </LogoBanner>
+          <div className='searchBarContainer'>
+            <div className='searchBar'>
+              <div className='searchIcon'>
+                <img className="searchIconImg" src={headerData.globalHeaderSearchIcon} alt={headerData.globalHeaderSearchIconAltText}/>
+              </div>
+            </div>
+          </div>
+        </HeaderContainer>
+      </HeaderBanner>
     </>
   );
 };

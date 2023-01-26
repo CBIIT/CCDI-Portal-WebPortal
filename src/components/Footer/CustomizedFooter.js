6c2da0c687057433a410eeb55d7c83db03642698
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-01-26 14:08:51 -0500

    implemented Footer upper part
    implemented Footer upper part

--- src/components/Footer/CustomizedFooter.js
+++ src/components/Footer/CustomizedFooter.js
@@ -39,7 +39,35 @@ const FooterLogo = styled.div`
     font-size: 12px;
     color: #393C3C;
   }
+`;
 
+const FooterLinksContainer = styled.div`
+  margin: 54px 87px 0 auto;
+  display: flex;
+
+  .footItem {
+    width: 253px; 
+  }
+
+  .footItemTitle {
+    font-family: Open Sans;
+    color: #393C3C;
+    font-weight: 700;
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
+
+  .footItemSubtitle {
+    margin-bottom: 10px;
+  }
+
+  .footItemLink {
+    font-family: Open Sans;
+    color: #393C3C;
+    font-weight: 600;
+    font-size: 12px;
+    text-decoration: none;
+  }
 `;
 
 const Footer = () => {
@@ -53,6 +81,31 @@ const Footer = () => {
                     <div className='logoLowerText'>at the National Institutes of Health</div>
                   </a>
                 </FooterLogo>
+                <FooterLinksContainer>
+                    {
+                        FooterData.link_sections.map((linkItem, itemidx) => {
+                            return (
+                                <div className='footItem'>
+                                    <div className='footItemTitle'>{linkItem.title}</div>
+                                    {
+                                        linkItem.items.map((item) => {
+                                            return (
+                                                <div className='footItemSubtitle'>
+                                                    {
+                                                        item.link.includes('http') ? 
+                                                        <a className='footItemLink' href={item.link} target="_blank" rel="noopener noreferrer">{item.text}</a>
+                                                        :
+                                                        <a className='footItemLink' href={item.link}>{item.text}</a>
+                                                    }
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </FooterLinksContainer>
             </FooterUpperContainer>
         </FooterContainer>
     </FooterStyled>

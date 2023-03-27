c5f61d253e889b644f3e2aaca9808eff1c771945
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-03-27 13:41:48 -0400

    Added timer
    Added timer

--- src/pages/landing/component/carousel4.js
+++ src/pages/landing/component/carousel4.js
@@ -5,7 +5,6 @@ import { carouselList } from '../../../bento/landingPageData'
 import exportIcon from '../../../assets/landing/Export_Icon.svg';
 import arrowIcon from '../../../assets/landing/arrow.svg';
 
-let cardIdx = 0;
 let timer = null;
 
 const HeroListContainer = styled.div`
@@ -104,99 +103,6 @@ const HeroList = styled.div`
     border-radius: 40px;
     overflow: hidden;
 
-    .carouselList {
-        width: 100%;
-        height: 131px;
-        -ms-overflow-style: none;
-        scrollbar-width: none; 
-        display: grid;
-    }
-
-    .carouselList::-webkit-scrollbar {
-        display: none;
-    }
-
-    .carouselList > * {
-        width: 100%;
-        flex-shrink: 0;
-        flex-grow: 1;
-      }
-
-    .listItem  {
-        display: flex;
-        position: relative;
-        width: 652px;
-        height: 102px;
-        margin: 9px 49px;
-        background: #F7F7F7;
-        box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.15);
-        border-radius: 20px;
-    }
-
-    .activeCard {
-        border-radius: 20px;
-        width: 700px;
-        height: 140px;
-        margin: 9px 25px;
-    }
-
-    .itemImgBox {
-        margin-left: 19px;
-    }
-
-    .itemImg {
-        width: 243px;
-        height: 102px;
-    }
-
-    .listItemContent {
-        color: #000000;
-        font-family: poppins;
-        font-weight: 400;
-        font-size: 22px;
-        line-height: 25px;
-        width: 329px;
-        text-decoration: none;
-        display: flex;
-        align-items: center;
-        letter-spacing: -0.01em;
-        margin-left: 41px;
-    }
-
-    .activeCard .itemImgBox {
-        margin-left: 30px;
-    }
-
-    .activeCard .itemImgBox .itemImg {
-        width: 270px;
-        height: 140px;
-    }
-
-    .activeCard .listItemContent {
-        color: #01828C;
-        font-family: poppins;
-        font-weight: 600;
-        font-size: 28px;
-        line-height: 30px;
-        width: 334px;
-        letter-spacing: -0.002em;
-        margin-left: 27px;
-    }
-
-    .exportIcon {
-        margin-top: 100px;
-        width: 24.8px;
-        height: 24.8px;
-    }
-
-    .exportContainer {
-        text-decoration: none;
-    }
-    .exportContainerInactive {
-        display: none;
-        text-decoration: none;
-    }
-
     .blurTop {
         position: absolute;
         background: linear-gradient(to bottom, #FFFFFF, 70%, transparent);
@@ -255,57 +161,43 @@ const HeroList = styled.div`
     }
 
     .carousel__item:nth-child(1) {
-        transform: translateY(-400%) scale(0.8);
+        transform: translateY(-300%) scale(0.9);
         opacity: 0;
         visibility: hidden;
     }
 
     .carousel__item:nth-child(2) {
-        transform: translateY(-300%) scale(0.8);
-        opacity: 0;
-        visibility: hidden;
-    }
-
-    .carousel__item:nth-child(3) {
-        transform: translateY(-200%) scale(0.8);
+        transform: translateY(-200%) scale(0.9);
         opacity: 0.8;
         visibility: visible;
     }
 
-    .carousel__item:nth-child(4) {
+    .carousel__item:nth-child(3) {
         transform: translateY(-100%) scale(0.9);
         opacity: 0.9;
         visibility: visible;
     }
 
-    .carousel__item:nth-child(5) {
+    .carousel__item:nth-child(4) {
         transform: translateY(0) scale(1);
         opacity: 1;
         visibility: visible;
-        color: red;
-        background: blue;
     }
 
-    .carousel__item:nth-child(6) {
+    .carousel__item:nth-child(5) {
         transform: translateY(100%) scale(0.9);
         opacity: 0.9;
         visibility: visible;
     }
 
-    .carousel__item:nth-child(7) {
-        transform: translateY(200%) scale(0.8);
+    .carousel__item:nth-child(6) {
+        transform: translateY(200%) scale(0.9);
         opacity: 0.8;
         visibility: visible;
     }
 
-    .carousel__item:nth-child(8) {
-        transform: translateY(300%) scale(0.8);
-        opacity: 0;
-        visibility: hidden;
-    }
-
     .carousel__item:last-child {
-        transform: translateY(400%) scale(0.8);
+        transform: translateY(300%) scale(0.9);
         opacity: 0;
         visibility: hidden;
     }
@@ -340,22 +232,12 @@ const HeroList = styled.div`
 `;
 
 const Carousel = () => {
-    const itemList = carouselList;
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const showCount = 5;
-    const distance = 120;
-    const [touchPosition, setTouchPosition] = useState(null);
-    const [transform, setTransform] = useState({ transform: "translateY(0%)" });
-    const [btnDisabled, setBtnDisabled] = useState(false);
     const isVisible = usePageVisibility();
 
     const startTimer = () => {
         timer = setInterval(() => {
-            const ts = `translateY(-${(cardIdx + 1) * distance}px)`;
-            cardIdx += 1;
-            setCurrentIndex(cardIdx);
-            setTransform({transform: ts, transition: ".5s ease-out"});
-        }, 300000000);
+            nextItem();
+        }, 3000);
     };
 
     const resetTimer = () => {
@@ -363,14 +245,20 @@ const Carousel = () => {
         startTimer();
     };
 
-    const nextSlide = () => {
+    const nextItem = () => {
         const list = document.getElementById("carouselList");
         const lastitem = list.lastChild;
         list.removeChild(lastitem);
         list.insertBefore(lastitem, list.firstChild);
     };
 
+    const nextSlide = () => {
+        resetTimer();
+        nextItem();
+    };
+
     const prevSlide = () => {
+        resetTimer();
         const list = document.getElementById("carouselList");
         const firstitem = list.firstChild;
         list.removeChild(firstitem);
@@ -380,59 +268,11 @@ const Carousel = () => {
     useEffect(() => {
         if (!isVisible) {
             clearInterval(timer);
-        }
-        const len = itemList.length;
-        if (itemList.length !== 0 && isVisible) {
-            cardIdx = Math.floor(Math.random() * len) + showCount;
-            const initialTs = `translateY(-${cardIdx * distance}px)`;
-            setCurrentIndex(cardIdx);
-            setTransform({transform: initialTs, transition: "none"});
+        } else {
             startTimer();
         }
         return () => clearInterval(timer);
     }, [isVisible]);
-
-    const handleTouchStart = (e) => {
-        const touchDown = e.touches[0].clientY;
-        setTouchPosition(touchDown);
-    };
-
-    const handleTouchMove = (e) => {
-        const touchDown = touchPosition;
-
-        if (touchDown === null) {
-            return;
-        }
-
-        const currentTouch = e.touches[0].clientY;
-        const diff = touchDown - currentTouch;
-
-        if (diff > 5) {
-            nextSlide();
-        }
-
-        if (diff < -5) {
-            prevSlide();
-        }
-
-        setTouchPosition(null);
-    };
-
-    const handleTransitionEnd = () => {
-        if (cardIdx === 0) {
-            cardIdx = itemList.length;
-            const initialTs = `translateY(-${cardIdx * distance}px)`;
-            setCurrentIndex(cardIdx);
-            setTransform({transform: initialTs, transition: "none"});
-        }
-
-        if (cardIdx === (itemList.length + showCount)) {
-            cardIdx = showCount;
-            const initialTs = `translateY(-${cardIdx * distance}px)`;
-            setCurrentIndex(cardIdx);
-            setTransform({transform: initialTs, transition: "none"});
-        }
-    };
 
     return (
         <HeroListContainer>
@@ -444,55 +284,10 @@ const Carousel = () => {
             </div>
             <div className="arrowLeft" />
             <div className="arrowRight" />
-            <HeroList onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+            <HeroList>
                 <div className='blurTop' />
                 <div className='blurBottom' />
                 <div className='activeFrame' onMouseEnter={() => clearInterval(timer)} onMouseLeave={()=>{resetTimer()}} />
-                {/* <div className="carouselList" onTransitionEnd={handleTransitionEnd} style={transform}>
-                    {itemList.slice(itemList.length - showCount).map((item, idx) => {
-                        const key = `carousel_${idx}_last_clone`;
-                        const isActiveCard = ( currentIndex + 2 ) === idx;
-                        const noBorderLine = ( currentIndex + 1 ) === idx;
-                        return (
-                            <div key={key} className={isActiveCard ? "listItem activeCard" : "listItem"} onMouseEnter={() => clearInterval(timer)} onMouseLeave={()=>{resetTimer()}}>
-                                <div className='itemImgBox'><img className='itemImg' src={item.img} alt="" width="243px" height="102px" /></div>
-                                <a className='listItemContent' href={item.link} target="_blank" rel="noopener noreferrer">{item.content}</a>
-                                <a className={isActiveCard ? "exportContainer" : "exportContainerInactive"} href={item.link} target="_blank" rel="noopener noreferrer">
-                                    <img className='exportIcon' src={exportIcon} alt=""/>
-                                </a>
-                            </div>
-                        );
-                    })}
-                    {itemList.map((item, idx) => {
-                        const key = `carousel_${idx}`;
-                        const isActiveCard = ( currentIndex + 2 ) === (idx + showCount);
-                        const noBorderLine = ( currentIndex + 1 ) === (idx + showCount);
-                        return (
-                            <div key={key} className={isActiveCard ? "listItem activeCard" : "listItem"} onMouseEnter={() => clearInterval(timer)} onMouseLeave={()=>{resetTimer()}}>
-                                <div className='itemImgBox'><img className='itemImg' src={item.img} alt="" /></div>
-                                <a className='listItemContent' href={item.link} target="_blank" rel="noopener noreferrer">{item.content}</a>
-                                <a className={isActiveCard ? "exportContainer" : "exportContainerInactive"} href={item.link} target="_blank" rel="noopener noreferrer">
-                                    <img className='exportIcon' src={exportIcon} alt=""/>
-                                </a>
-                            </div>
-                        );
-                    })}
-                    {itemList.slice(0, showCount).map((item, idx) => {
-                        const key = `carousel_${idx}_first_clone`;
-                        const isActiveCard = ( currentIndex + 2 ) === (idx + itemList.length + showCount);
-                        const noBorderLine = ( currentIndex + 1 ) === (idx + itemList.length + showCount);
-                        return (
-                            <div key={key} className={isActiveCard ? "listItem activeCard" : "listItem"} onMouseEnter={() => clearInterval(timer)} onMouseLeave={()=>{resetTimer()}}>
-                                <div className='itemImgBox'><img className='itemImg' src={item.img} alt="" width="243px" height="102px" /></div>
-                                <a className='listItemContent' href={item.link} target="_blank" rel="noopener noreferrer">{item.content}</a>
-                                <a className={isActiveCard ? "exportContainer" : "exportContainerInactive"} href={item.link} target="_blank" rel="noopener noreferrer">
-                                    <img className='exportIcon' src={exportIcon} alt=""/>
-                                </a>
-                            </div>
-                        );
-                    })}
-                </div> */}
-                {/* <div class='wrapper'> */}
                     <div id="carouselList" class='carousel'>
                         <div class='carousel__item'>
                             <div class='carousel__item-head'>
@@ -557,26 +352,7 @@ const Carousel = () => {
                                 <p>Unicode: U+1F988</p>
                             </div>
                         </div>
-                        <div class='carousel__item'>
-                            <div class='carousel__item-head'>
-                                🐙
-                            </div>
-                            <div class='carousel__item-body'>
-                                <p class='title'>8</p>
-                                <p>Unicode: U+1F419</p>
-                            </div>
-                        </div>
-                        <div class='carousel__item'>
-                            <div class='carousel__item-head'>
-                                🐚
-                            </div>
-                            <div class='carousel__item-body'>
-                                <p class='title'>9</p>
-                                <p>Unicode: U+1F41A</p>
-                            </div>
-                        </div>
                     </div>
-                {/* </div> */}
 
             </HeroList>
         </HeroListContainer>

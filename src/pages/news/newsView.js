8b51221007bfdda882dca3f51e78d96e6db49917
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-04-12 17:10:10 -0400

    Added tablist
    Added tablist

--- src/pages/news/newsView.js
+++ src/pages/news/newsView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import newsImg from '../../assets/news/News_Header.jpg';
 
@@ -6,7 +6,7 @@ const NewsContainer = styled.div`
   width: 1440px;
   margin: 0 auto;
 
-  .newsHeader{
+  .newsHeader {
     width: 1142px;
     height: 214px;
     margin: 0 auto;
@@ -20,12 +20,43 @@ const NewsContainer = styled.div`
     letter-spacing: 0.02em;
     color: #FFFFFF;
   }
+
+  .tabList {
+    display: flex;
+    margin: 17px 0 40px 140px;
+  }
+
+  .tabListItem {
+    font-family: 'Poppins';
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 16px;
+    color: #298085;
+    margin-left: 60px;
+  }
+
+  .tabListItem:hover {
+    cursor: pointer;
+  }
 `;
 
 const NewsView = () => {
+  const [selectedTab, setSelectedTab] = useState("All");
+  const newsTabList = ['All', 'Spotlights', 'News', 'News', 'Site Updates', 'Staff Recipes'];
   return (
     <NewsContainer>
       <div className='newsHeader'>Hub News and Updates</div>
+      <div className='tabList'>
+        {
+          newsTabList.map((newsTabItem, idx) => {
+            const tabkey = `tabkey_${idx}`;
+            return (
+            <div key={tabkey} className='tabListItem' onClick={(() => setSelectedTab(newsTabItem))}>{newsTabItem}</div>
+            )
+          })
+        }
+      </div>
+      <div>{selectedTab}</div>
     </NewsContainer>
   )
 };

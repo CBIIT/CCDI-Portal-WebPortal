e959f60064e26cbf8aa66c1138089943d67f92d6
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-04-12 16:40:40 -0400

    Added newsHeader
    Added newsHeader

--- src/pages/news/newsView.js
+++ src/pages/news/newsView.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import newsImg from '../../assets/news/News_Header.jpg';
+
+const NewsContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+
+  .newsHeader{
+    width: 1142px;
+    height: 214px;
+    margin: 0 auto;
+    background-image: url(${newsImg});
+    border-radius: 0px 0px 20px 20px;
+    font-family: 'Poppins';
+    font-weight: 600;
+    font-size: 35px;
+    line-height: 214px;
+    text-align: center;
+    letter-spacing: 0.02em;
+    color: #FFFFFF;
+  }
+`;
 
 const NewsView = () => {
   return (
-    <div style={{marginTop: "140px", height: "250px", fontSize: "100px"}}>News</div>
+    <NewsContainer>
+      <div className='newsHeader'>Hub News and Updates</div>
+    </NewsContainer>
   )
 };
 

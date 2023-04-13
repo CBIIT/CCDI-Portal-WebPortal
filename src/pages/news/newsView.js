dac131936543433cd1d2e2d1bea59d602f90b6e3
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-04-13 16:23:59 -0400

    Added newsItem
    Added newsItem

--- src/pages/news/newsView.js
+++ src/pages/news/newsView.js
@@ -23,7 +23,7 @@ const NewsContainer = styled.div`
 
   .tabList {
     display: flex;
-    margin: 17px 0 40px 140px;
+    margin: 17px 0 40px 150px;
   }
 
   .tabListItem {
@@ -42,6 +42,7 @@ const NewsContainer = styled.div`
     line-height: 16px;
     color: #0A5E63;
     margin-left: 60px;
+    padding-bottom: 5px;
     border-bottom: 3px solid #0A5E63;
   }
 
@@ -55,6 +56,52 @@ const NewsContainer = styled.div`
 
   .tabListItem:hover {
     cursor: pointer;
+  }
+
+  .newsList {
+    width: 1047px;
+    margin: 0 auto;
+  }
+
+  .newsItem {
+    display: flex;
+    height: 248px;
+    border: 0.25px solid #78AEB3;
+    border-radius: 0px 20px;
+    margin-bottom: 29px;
+    padding: 23px 32px 0 38px;
+  }
+
+  .newsItemTextContainer {
+    width: 76%;
+  }
+
+  .newsItemTitle {
+    font-family: 'Poppins';
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+    color: #0095A2;
+    margin-bottom: 8px;
+  }
+
+  .newsItemDate {
+    font-family: 'Inter';
+    font-weight: 300;
+    font-size: 13px;
+    line-height: 24px;
+    text-transform: uppercase;
+    color: #000000;
+    margin-bottom: 8px;
+  }
+
+  .newsItemContent {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 16px;
+    line-height: 24px;
+    color: #000000;
   }
 `;
 
@@ -74,7 +121,15 @@ const NewsView = () => {
           })
         }
       </div>
-      <div>{selectedTab}</div>
+      <div className='newsList'>
+        <div className='newsItem'>
+          <div className='newsItemTextContainer'>
+            <div className='newsItemTitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud exercitation ullamco</div>
+            <div className='newsItemDate'>APRIL 4, 2023</div>
+            <div className='newsItemContent'>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
+        </div>
+      </div>
     </NewsContainer>
   )
 };

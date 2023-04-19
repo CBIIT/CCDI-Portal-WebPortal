6158fc121e449ba8c619b8d2ae58d882b7e9406c
David-YuWei <ywwei85@gmail.com>
2023-04-19 12:57:09 -0400

    Update frontend framework to solve routing issue

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { CssBaseline } from '@material-ui/core';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Routes, } from 'react-router-dom';
 import Header from '../Header/CustomizedHeader';
 import NavBar from '../NavBar/CustomizedNavBar';
 import Footer from '../Footer/CustomizedFooter';
 import Home from '../../pages/landing/landingView';
 import About from '../../pages/about/aboutView';
 import News from '../../pages/news/newsView';
-import Ccdi from '../../pages/ccdi/ccdiView';
 import Error from '../../pages/error/Error';
 import Search from '../../pages/search/searchView';
 import ScrollButton from '../ScrollButton/ScrollButtonView';
@@ -17,22 +16,19 @@ const Layout = () => {
     return (
     <>
       <CssBaseline />
-      <Router>
         <Header />
         <NavBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/news" component={News} />
-          <Route exact path="/ccdi" component={Ccdi} />
-          <Route path="/sitesearch" component={Search} />
-          <Route path="/newsdetail/:id" component={NewsDetail} />
-          <Route component={Error} />
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/sitesearch" element={<Search />} />
+          <Route path="/newsdetail/:id" element={<NewsDetail />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
         <Footer />
         <ScrollButton />
-      </Router>
     </>
     )
 }
--- src/pages/ccdi/ccdiView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ccdiView = () => {
-  return (
-    <div style={{marginTop: "140px", height: "250px", fontSize: "100px"}}>CCDI</div>
-  )
-};
-
-export default ccdiView;

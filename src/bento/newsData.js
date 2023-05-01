2b95c3585fb5c442a8a5d3ddc41306a7500087f1
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-05-01 11:41:12 -0400

    PORTAL-284: OCPL News Text Update
    Update Slug and Highlight for - MCI, CCDC, CCDI Symposium
    Remove: Cancer Plan entry.

--- src/bento/newsData.js
+++ src/bento/newsData.js
@@ -1,10 +1,8 @@
 import newsCCDC from '../assets/news/News_CCDC.png';
 import newsMolecular from '../assets/news/News_Molecular.png';
 import newsSymposium from '../assets/news/News_Symposium.png';
-import newsCancerPlan from '../assets/news/News_CancerPlan.png';
 import newsMolecularDetail from '../assets/news/News_MCI_Detail.png';
 import newsSymposiumDetail from '../assets/news/News_Symposium_Detail.png';
-import newsCancerPlanDetail from '../assets/news/News_CancerPlan_Detail.png';
 import updateImgCCDC from '../assets/landing/Updates_CCDC.png';
 import updateImgMolecular from '../assets/landing/Updates_Molecular.png';
 import updateImgSymposium from '../assets/landing/Updates_Symposium.png';
@@ -14,8 +12,8 @@ export const newsList = [
         id: 'ccdc',
         title: 'Childhood Cancer Data Catalog April Update ',
         date: 'APRIL 19, 2023',
-        slug: 'The <a href="https://datacatalog.ccdi.cancer.gov/" target="_blank" rel="noopener noreferrer">Childhood Cancer Data Catalog</a> announced an update which includes 1 new resource, 8 new datasets, and many other updates.',
-        highlight: 'The <a href="https://datacatalog.ccdi.cancer.gov/" target="_blank" rel="noopener noreferrer">Childhood Cancer Data Catalog</a> is pleased to announce an April update which includes the addition of the Xena Browser, a powerful online exploration tool that allows users to investigate functional genomic data sets for correlations between genomic and/or phenotypic variables, as a new analytic tool resource.  Additionally, 7 new datasets have been incorporated into 8 existing resources.<a href="https://datacatalog.ccdi.cancer.gov/" rel="noreferrer noopener" target="_blank">Explore the Catalog</a> to check out all the changes.',
+        slug: 'The update includes one new resource, eight new datasets, and many other changes',
+        highlight: 'The <a href="https://datacatalog.ccdi.cancer.gov/" target="_blank" rel="noopener noreferrer">Childhood Cancer Data Catalog</a> is pleased to announce the addition of the Xena Browser, a powerful online exploration tool that allows users to investigate functional genomic data sets for correlations between genomic and/or phenotypic variables. Also, seven new datasets have been incorporated into eight existing resources. <a href="https://datacatalog.ccdi.cancer.gov/" rel="noreferrer noopener" target="_blank">Explore the Catalog</a> to check out all the changes.',
         fullText: '<p>The National Cancer Institute is pleased to announce an update to the Childhood Cancer Data Catalog website: <a href="https://datacatalog.ccdi.cancer.gov/" rel="noreferrer noopener" target="_blank">https://datacatalog.ccdi.cancer.gov/</a>.&nbsp;&nbsp;</p>'
         +'<p>&nbsp;</p>'
         +'<p>This update includes the addition of the Xena Browser as a new analytic tool resource.&nbsp; Xena Browser is an online exploration tool that allows users to investigate functional genomic data sets for correlations between genomic and/or phenotypic variables.&nbsp; Additionally, 7 new datasets have been incorporated into 8 existing resources, with 5 of these coming from The Cancer Imaging Archive (TCIA).&nbsp;</p>'
@@ -52,10 +50,10 @@ export const newsList = [
     },
     {
         id: 'molecular',
-        title: 'Molecular Characterization Initiative enrolled more than 1,000 participants',
+        title: 'Molecular Characterization Initiative',
         date: 'APRIL 5, 2023',
-        slug: 'The <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/data-ecosystem/molecular-characterization?cid=eb_govdel" target="_blank" rel="noopener noreferrer">CCDI’s MCI</a> enrolled & returned results for more than 1,000 children & AYAs newly diagnosed with CNS, STS, & rare cancers.',
-        highlight: 'The <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/data-ecosystem/molecular-characterization?cid=eb_govdel" target="_blank" rel="noopener noreferrer">CCDI Molecular Characterization Initiative</a> (MCI)  enrolled over 1,000 children, adolescent, and young adults (AYAs) diagnosed with brain, soft tissue sarcomas  and certain types of rare cancers. This increases the accessibility to clinical genomic testing, which can aid healthcare providers to confirm diagnoses and in planning their treatment.',
+        slug: 'Genomics and clinical data for MCI participants is available at NCI’s Cancer Data Service and accessible through CGC.',
+        highlight: '<p>De-identified molecular characterization, clinical reports, and demographic & phenotypic data from participants in the <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/data-ecosystem/molecular-characterization?cid=eb_govdel" rel="noreferrer noopener" target="_blank">CCDI Molecular Characterization Initiative</a> (MCI) has been released, <a href="https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=phs002790.v2.p1" rel="noreferrer noopener" target="_blank">phs00290.v2.p1</a> and could be requested through dbGaP <a href="https://www.youtube.com/watch?v=m0xp_cCO7kA" rel="noreferrer noopener" target="_blank">controlled-access process</a>. There is data associated with 1145 participants and additional data from MCI will be released each month.  The data are accessible through Cancer genomics Cloud for in secure workspaces and facilitate uploading of researchers’ own data for aggregated/federated analysis.</p>',
         fullText: '<p>The <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/data-ecosystem/molecular-characterization?cid=eb_govdel" rel="noreferrer noopener" target="_blank">CCDI Molecular Characterization Initiative</a> (MCI) achieved a significant milestone in March, enrolling over 1,000 newly diagnosed children, adolescent, and young adults (AYAs) with certain types of cancer. This milestone means that over 1,000 eligible patients now have access to more precise diagnoses, which can aid healthcare providers in planning their treatment.&nbsp;</p>'
         +'<p>&nbsp;</p>'
         +'<p>Enrollment continues, free of charge, for children and AYAs newly diagnosed with central nervous system tumors, soft tissue sarcomas, and rare tumors who are receiving care from a Children&rsquo;s Oncology Group&ndash;affiliated hospital.&nbsp;</p>'
@@ -71,10 +69,10 @@ export const newsList = [
     },
     {
         id: 'symposium',
-        title: '2023 CCDI Symposium ',
-        date: 'APRIL 4, 2023',
-        slug: 'More than 800 people attended the <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/events-webinars/past-symposiums">CCDI Annual Symposium</a>, on March 24, 2023 showing their support for childhood cancer research.',
-        highlight: 'The Childhood Cancer Data Initiative Annual Symposium, hosted by NCI on March 24, 2023, brought together over 800 members of the research, clinical, survivor, and family communities. The event focused on the potential of enhanced data connection and sharing to address current challenges and opportunities in childhood cancer research. <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/events-webinars/symposium-0" target="_blank" rel="noopener noreferrer">View the event presentation</a> and <a href="https://videocast.nih.gov/watch=49171" target="_blank" rel="noopener noreferrer">watch the recording</a>.',
+        title: 'CCDI Symposium features Data Ecosystem progress',
+        date: 'MARCH 24, 2023',
+        slug: 'More than 800 people came together to discuss CCDI progress, including in the Data Ecosystem.',
+        highlight: 'NCI hosted the CCDI Symposium on March 24, 2023, which brought together hundreds of members of the childhood cancer community. The event featured presentations on CCDI Data Ecosystem progress by Drs. Anthony Kerlavage and Subhashini Jagu. <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/events-webinars/symposium-0" target="_blank" rel="noopener noreferrer">View the event presentation</a> and <a href="https://videocast.nih.gov/watch=49171" target="_blank" rel="noopener noreferrer">watch the recording</a>.',
         fullText: '<p>On March 24, 2023, NCI hosted the <a href="https://www.cancer.gov/research/areas/childhood/childhood-cancer-data-initiative/events-webinars/past-symposiums" rel="noreferrer noopener" target="_blank">Childhood Cancer Data Initiative Annual Symposium</a>. Researchers, clinicians, survivors, and families from the community gathered to discuss how enhanced data connection and sharing can address current issues and possibilities in childhood cancer research. Dialogue between speakers and attendees, both virtual and in-person, celebrated CCDI&#39;s accomplishments to date and identified challenges and priorities that lie ahead.  Highlights included discussion of linking patients to disparate data sets, using tumor genomic data to create precision oncology clinical trials, extracting data from electronic health records, and other topics important for the pediatrics cancer community.&nbsp;</p>'
         +'<p>&nbsp;</p>'
         +'<p>Following the symposium, six breakout sessions were held to continue the symposium&rsquo;s discussion:&nbsp;</p><ol start="1"><li><p>Molecular Characterization Initiative and the potential for future cohort studies&nbsp;</p></li></ol><ol start="2"><li><p>Patient and family perspectives on computable consent and CCDI Participant Index&nbsp;</p></li></ol><ol start="3"><li><p>Electronic health record data extraction: current status and continuing challenges&nbsp;</p></li></ol><ol start="4"><li><p>External controls for pediatric cancer clinical trials&nbsp;</p></li></ol><ol start="5"><li><p>Collaborations and transformative research opportunities using data available through the CCDI ecosystem&nbsp;</p></li></ol><ol start="6"><li><p>Observational studies and novel interventional approaches for rare pediatric cancers&nbsp;</p></li></ol>'
@@ -84,23 +82,5 @@ export const newsList = [
         img: newsSymposium,
         updateImg: updateImgSymposium,
         detailImg: newsSymposiumDetail,
-    },
-    {
-        id: 'national_cancer_plan',
-        title: 'National Cancer Plan',
-        date: 'APRIL 3, 2023',
-        slug: 'NCI released the National Cancer Plan to engage the broadest possible community to work together to end cancer as we know it.',
-        highlight: 'National Cancer Institute (NCI) has released a <a href="https://nationalcancerplan.cancer.gov/?cid=eb_govdel_allstaff_ncp" target="_blank" rel="noopener noreferrer">National Cancer Plan</a>, a long-term, ambitious framework to end cancer as we know it. The Plan represents a coordinated national response to achieve the goals of the <a href="https://www.cancer.gov/research/key-initiatives/moonshot-cancer-initiative" target="_blank" rel="noopener noreferrer">Cancer Moonshot</a> and deliver better cancer outcomes. The plan offers something for everyone who wants to join in creating a society where more cancers are prevented and every person with cancer lives a full and active life.',
-        fullText: '<p>National Cancer Institute (NCI) has released a <a href="https://nationalcancerplan.cancer.gov/?cid=eb_govdel_allstaff_ncp" rel="noreferrer noopener" target="_blank">National Cancer Plan</a>, a long-term, ambitious framework to end cancer as we know it. The Plan represents a coordinated national response to achieve the goals of the <a href="https://www.cancer.gov/research/key-initiatives/moonshot-cancer-initiative" rel="noreferrer noopener" target="_blank">Cancer Moonshot</a> and deliver better cancer outcomes to all people.&nbsp;</p>'
-        +'<p>The development of the National Cancer Plan was made in part due to the contributions of a broad coalition of partners, including the National Cancer Advisory Board, NCI&rsquo;s Council of Research Advocates and numerous stakeholders across NIH, HHS, the White House Office of Science and Technology Policy, and the Office of the First Lady. The plan offers something for everyone who wants to join in creating a society where more cancers are prevented and every person with cancer lives a full and active life.&nbsp;</p>'
-        +'<p>&nbsp;</p>'
-        +'<p>At its core, the National Cancer Plan features eight goals that build on our current knowledge and describe an ideal future state&mdash;essential components to ending cancer as we know it.&nbsp;&nbsp;</p>'
-        +'<ol start="1"><li><p>Prevent Cancer&nbsp;</p></li></ol><ol start="2"><li><p>Detect Cancers Early&nbsp;</p></li></ol><ol start="3"><li><p>Develop Effective Treatments&nbsp;</p></li></ol><ol start="4"><li><p>Eliminate Inequities&nbsp;</p></li></ol><ol start="5"><li><p>Deliver Optimal Care&nbsp;</p></li></ol><ol start="6"><li><p>Engage Every Person&nbsp;</p></li></ol><ol start="7"><li><p>Maximize Data Utility&nbsp;</p></li></ol><ol start="8"><li><p>Optimize the Workforce&nbsp;</p></li></ol>'
-        +'<p>&nbsp;</p>'
-        +'<p>Everything at NCI helps to drive progress toward these goals. The National Cancer Plan helps to describe ongoing and emerging initiatives and invites others to do the same.&nbsp; The better NCI and its partners can identify gaps in cancer prevention, detection, treatment, and care, the better all communities are able to work across society to address them.&nbsp;</p>',
-        type: 'News & Other',
-        img: newsCancerPlan,
-        updateImg: '',
-        detailImg: newsCancerPlanDetail,
     },
 ]

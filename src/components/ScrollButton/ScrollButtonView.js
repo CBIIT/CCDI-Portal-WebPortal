2223ec56b5603a39e148b52d2880304c20b6b246
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-06-13 16:24:25 -0400

    fixed Scroll button
    fixed Scroll button

--- src/components/ScrollButton/ScrollButtonView.js
+++ src/components/ScrollButton/ScrollButtonView.js
@@ -1,30 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import "./ScrollButtonStyles.css";
+import styled from 'styled-components';
+import { ReactComponent as ScrollToTopButton } from '../../assets/icons/Scroll_to_top.svg';
 
+const Button = styled.div`
+   position: fixed; 
+   right: 0;
+   bottom: 0;
+   height: 80px;
+   width: 80px;
+   z-index: 10;
+   cursor: pointer;
+   transition: all 0.25s ease-out;
+`;
 
 const ScrollButton = () => {
-    const [scroll, setScroll] = useState(0);
+    const [visible, setVisible] = useState(false);
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 200) {
+            setVisible(true);
+        } else if (scrolled <= 200) {
+            setVisible(false);
+        }
+    };
 
-    const updateScroll = () => {
-        setScroll(window.scrollY);
-    }
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
 
     useEffect(() => {
-        window.addEventListener('scroll', updateScroll);
+        window.addEventListener('scroll', toggleVisible);
     }, []);
 
     return (
-        <a id="stt" href="#top"
-            style={scroll < 200 ? {
-                opacity: 0,
-                visibility: "hidden"
-            }
-                : {
-                    visibility: "visible",
-                    opacity: 1,
-                }}>
-            <span id="stt-span">BACK TO TOP</span>
-        </a>
+        <Button onClick={scrollToTop}
+                style={visible 
+                    ? {
+                        opacity: 1,
+                        visibility: "visible",
+                    } 
+                    : {
+                        opacity: 0,
+                        visibility: "hidden",
+                    }}>
+            <ScrollToTopButton />
+        </Button>
     );
 };
 

97939a18622682e3f8fe94e34a649bd5261aec74
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-06-14 13:36:57 -0400

    commented out outline on navbar
    commented out outline on navbar

--- src/components/ResponsiveHeader/HeaderTablet.js
+++ src/components/ResponsiveHeader/HeaderTablet.js
@@ -45,10 +45,10 @@ const HeaderContainer = styled.div`
         cursor: pointer;
     }
 
-    .menuButton:active {
-        outline: 0.25rem solid #2491ff;
-        outline-offset: 0.25rem
-    }
+    // .menuButton:active {
+    //     outline: 0.25rem solid #2491ff;
+    //     outline-offset: 0.25rem
+    // }
 `;
 
 const NavMobileContainer = styled.div`
@@ -92,10 +92,10 @@ const MenuArea = styled.div`
         cursor: pointer;
     }
 
-    .closeIconImg: active {
-        outline: 0.25rem solid #2491ff;
-        outline-offset: 0.5rem
-    }
+    // .closeIconImg:active {
+    //     outline: 0.25rem solid #2491ff;
+    //     outline-offset: 0.5rem
+    // }
 
     .backButton {
         font-family: Open Sans;
@@ -111,10 +111,10 @@ const MenuArea = styled.div`
         cursor: pointer;
     }
 
-    .backButton:active {
-        outline: 0.25rem solid #2491ff;
-        outline-offset: 0.5rem;
-    }
+    // .backButton:active {
+    //     outline: 0.25rem solid #2491ff;
+    //     outline-offset: 0.5rem;
+    // }
 
     .navMobileContainer {
         padding: 24px 0 0 0;
@@ -141,9 +141,9 @@ const MenuArea = styled.div`
         background-color: #f9f9f7;
     }
 
-    .navMobileItem:active {
-        outline: 0.25rem solid #2491ff;
-    }
+    // .navMobileItem:active {
+    //     outline: 0.25rem solid #2491ff;
+    // }
 
     .SubItem {
         padding-left: 24px;

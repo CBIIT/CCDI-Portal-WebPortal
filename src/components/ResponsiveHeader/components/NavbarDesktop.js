6beb81f6f9260269213bd7af205effc63eb220ad
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-06-16 14:01:40 -0400

    Changed navbar color from green to blue
    Changed navbar color from green to blue

--- src/components/ResponsiveHeader/components/NavbarDesktop.js
+++ src/components/ResponsiveHeader/components/NavbarDesktop.js
@@ -73,16 +73,16 @@ const LiSection = styled.li`
 
   .navText:hover {
     cursor: pointer;
-    color: #298085;
-    border-bottom: 4px solid #298085;
+    color: #3A75BD;
+    border-bottom: 4px solid #3A75BD;
 
     ::after {
       content: "";
       display: inline-block;
       width: 6px;
       height: 6px;
-      border-bottom: 1px solid #298085;
-      border-left: 1px solid #298085;
+      border-bottom: 1px solid #3A75BD;
+      border-left: 1px solid #3A75BD;
       margin: 0 0 4px 8px;
       transform: rotate(-45deg);
       -webkit-transform: rotate(-45deg);
@@ -103,7 +103,7 @@ const LiSection = styled.li`
 
   .clicked {
     color: #FFFFFF;
-    background: #1A5255;
+    background: #1F4671;
   }
 
   .clicked::after {
@@ -115,7 +115,7 @@ const LiSection = styled.li`
   }
 
   .clicked:hover {
-    border-bottom: 4px solid #1A5255;
+    border-bottom: 4px solid #1F4671;
     color: #FFFFFF;
 
     ::after {
@@ -155,7 +155,7 @@ const LiSection = styled.li`
     margin: 0 45px 0 5px;
     padding: 0 15px;
     user-select:none;
-    background: #1A5255;
+    background: #1F4671;
   }
 `;
 
@@ -163,7 +163,7 @@ const Dropdown = styled.div`
     top: 60.5px;
     left: 0;
     width: 100%;
-    background: #1A5255;
+    background: #1F4671;
     z-index: 1100;
     position: absolute;
     // visibility: hidden;
@@ -178,7 +178,7 @@ const DropdownContainer = styled.div`
     max-width: 1420px;
 
     .dropdownList {
-      background: #1A5255;
+      background: #1F4671;
       display: grid;
       grid-column-gap: 2%;
       grid-template-columns: 32% 32% 32%;
@@ -205,8 +205,8 @@ const DropdownContainer = styled.div`
 
 
 const activeStyle = {
-  color: '#298085',
-  borderBottom: '4px solid #298085',
+  color: '#3A75BD',
+  borderBottom: '4px solid #3A75BD',
 };
 
 const dropdownInvisibleStyle = {

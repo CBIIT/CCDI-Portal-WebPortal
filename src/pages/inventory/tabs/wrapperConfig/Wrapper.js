b7648185ffe5078cee186807530ea3f29a8dd7f8
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-08-14 12:11:10 -0400

    Added css style for ADD ALL FILTERED FILES
    Added css style for ADD ALL FILTERED FILES

--- src/pages/inventory/tabs/wrapperConfig/Wrapper.js
+++ src/pages/inventory/tabs/wrapperConfig/Wrapper.js
@@ -25,7 +25,7 @@ export const wrapperConfig = [{
   clsName: 'container_header',
   items: [
     {
-      title: 'ADD ALL FILES',
+      title: 'ADD ALL FILTERED FILES',
       clsName: 'add_all_button',
       type: types.BUTTON,
       role: btnTypes.ADD_ALL_FILES,
@@ -52,6 +52,15 @@ export const wrapperConfig = [{
   size: 'xl',
   clsName: 'container_footer',
   items: [
+    {
+      title: 'ADD ALL FILTERED FILES',
+      clsName: 'add_all_button',
+      type: types.BUTTON,
+      role: btnTypes.ADD_ALL_FILES,
+      btnType: btnTypes.ADD_ALL_FILES,
+      conditional: false,
+      alertMessage,
+    },
     {
       title: 'ADD SELECTED FILES',
       clsName: 'add_selected_button',

cacfb44c1c68ec4c32edbaf4324a46c7b809402f
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-08-22 17:37:22 -0400

    Added cart textContainer

--- src/pages/cart/cartView.js
+++ src/pages/cart/cartView.js
@@ -37,6 +37,9 @@ const CartView = (props) => {
     <Grid className={classes.backgroundContainer}>
       <Grid item xs={12}>
         <div className={classes.myFilesWrapper}>
+          <div className={classes.textContainer}>
+            To access and analyze files: select and remove unwanted files, click the “Download File Manifest” button, and upload the resulting Manifest file to your <a href="https://www.cancergenomicscloud.org/" target="_blank" rel="noopener noreferrer">Seven Bridges Genomics</a> account.
+          </div>
           <CartWrapper
             classes={classes}
             queryVariables={variables}
--- src/pages/cart/cartView.style.js
+++ src/pages/cart/cartView.style.js
@@ -5,6 +5,7 @@ export default () => ({
     paddingBottom: '80px',
   },
   myFilesWrapper: {
+    position: 'relative',
     border: '#4555AB 4px solid',
     borderRadius: '35px',
     marginLeft: '3%',
@@ -13,6 +14,22 @@ export default () => ({
     background: 'white',
     paddingRight: '3%',
     paddingLeft: '3%',
+  },
+  textContainer: {
+    position: 'absolute',
+    top: '90px',
+    left: '70px',
+    width: '520px',
+    fontFamily: 'Inter',
+    fontSize: '16px',
+    fontWeight: '400',
+    color: '#000000',
+    zIndex: '2',
+    '& a': {
+      fontFamily: 'Poppins',
+      fontWeight: '600',
+      color: '#004C73',
+    },
   },
   customTooltip: {
     border: '#03A383 1px solid',

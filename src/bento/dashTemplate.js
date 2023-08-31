92fb504a25517d59b9a756a668c47d18b1640358
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2023-08-30 20:06:22 -0400

    PORTAL-634: Inventory Explore Infographics: Missing unit specification for age ranges in the Diagnosis Age histogram chart (#197)
    * PORTAL-634: Inventory Explore Infographics: Missing unit specification for age ranges in the Diagnosis Age histogram chart
    
    The title of the histogram chart should clearly specify the unit as "Age at Diagnosis(years)", and the age ranges can remain as-is ("5-9", "10-14", "15-19") under this title.
    The title of the diagnosis donut chart shows as "Diagnosis (ICD-O Morphology)"
    
    * update title Diagnosis (ICD-O)

--- src/bento/dashTemplate.js
+++ src/bento/dashTemplate.js
@@ -784,13 +784,13 @@ export const widgetConfig = [
   },
   {
     type: 'donut',
-    title: 'Diagnosis',
+    title: 'Diagnosis (ICD-O)',
     dataName: 'participantCountByDiagnosis',
     sliceTitle: 'Participants',
   },
   {
     type: 'bar',
-    title: 'Diagnosis Age',
+    title: 'Age at Diagnosis (years)',
     dataName: 'participantCountByDiagnosisAge',
     width: '100%',
     height: 210,

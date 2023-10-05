c68503785ed380eda50d33e8ef3b3bb74196348a
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2023-10-05 12:15:40 -0400

    PORTAL-793: Spelling mistake for the column name "Number of Participants" in studies tab (#248)
    Fixed fypo

--- src/bento/tableDownloadCSV.js
+++ src/bento/tableDownloadCSV.js
@@ -111,7 +111,7 @@ query studyOverview($study_id: [String], $offset: Int = 0, $first: Int = 1000, $
 
 export const customStudyTabDownloadCSV = {
   keysToInclude: ['study_id', 'pubmed_id', 'grant_id', 'phs_accession', 'study_short_title', 'personnel_name', 'number_of_participants', 'diagnosis', 'number_of_samples', 'anatomic_site', 'number_of_files', 'file_type'],
-  header: ['Study ID', 'PubMed ID', 'Grant ID', 'Study Accession', 'Study Short Title', 'Principle Investigator(s)', 'Number of Particpants', 'Diagnosis', 'Number of Samples', 'Diagnosis Anatomic Site', 'Number of Files', 'File Type'],
+  header: ['Study ID', 'PubMed ID', 'Grant ID', 'Study Accession', 'Study Short Title', 'Principle Investigator(s)', 'Number of Participants', 'Diagnosis', 'Number of Samples', 'Diagnosis Anatomic Site', 'Number of Files', 'File Type'],
   query: GET_STUDY_TAB,
   apiVariable: 'studyOverview',
   fileName: 'tableDownload',

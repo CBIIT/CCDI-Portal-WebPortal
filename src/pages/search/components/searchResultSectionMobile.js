87744b7ce5499619498002513d997fda9a9acc87
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2024-01-19 23:00:11 -0500

    cleared up searchResultSectionMobile.js
    deleted useless pagination part

--- src/pages/search/components/searchResultSectionMobile.js
+++ src/pages/search/components/searchResultSectionMobile.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import {
   withStyles, Grid,
 } from '@material-ui/core';
-import Pagination from '@material-ui/lab/Pagination';
 import Components from './component';
 import client from '../../../utils/graphqlClient';
 import {
@@ -31,13 +30,11 @@ const useOutsideAlerter = (ref) => {
 function SearchPagination({
   datafield, classes, searchText, count, isPublic,
 }) {
-  const sizelist = [5,10];
+  const pageSize = 5;
   const [isFetching, setIsFetching] = useState(false);
   const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(sizelist[0]);
   const [data, setdata] = useState([]);
   // const [loading, setLoading] = useState(false);
-  const [pageListVisible, setPageListVisible] = useState(0);
   const perPageSelection = useRef(null);
   const [fetchMore, setFetchMore] = useState(true);
   useOutsideAlerter(perPageSelection);
@@ -87,23 +84,7 @@ function SearchPagination({
     setPage(1);
     setdata([]);
     onChange(searchText);
-  }, [searchText, datafield, pageSize]);
-
-  const onNext = () => {
-    if (page < Math.ceil(count / pageSize)) {
-      onChange(searchText, page + 1);
-      setPage(page + 1);
-      // scrollToTop();
-    }
-  };
-
-  const onPrevious = () => {
-    if (page > 1) {
-      onChange(searchText, page - 1);
-      setPage(page - 1);
-      // scrollToTop();
-    }
-  };
+  }, [searchText, datafield]);
 
   // const scrollToTop = () => {
   //   window.scrollTo({
@@ -112,25 +93,12 @@ function SearchPagination({
   //   });
   // };
 
-  const handleChangePage = (event, newPage) => {
-    console.log("newpage:", newPage)
-    onChange(searchText, newPage);
-    setPage(newPage);
-    
-    // scrollToTop();
-  };
-
   const handleLoadMore = () => {
     console.log("newpage:", page)
     onChange(searchText, page+1);
     setPage(page+1);
     
     // scrollToTop();
-  };
-
-  const onPageSizeClick = (e) => {
-    setPageSize(Number(e.target.innerText));
-    setPageListVisible(!pageListVisible)
   };
 
   const renderCards = () => {
@@ -177,7 +145,7 @@ function SearchPagination({
       <div className={classes.totalResults}>
         <span className={classes.totalCount}>{count}</span>
         {' '}
-        hello
+        results
       </div>
       ) }
       <Grid className={classes.subsection}>
@@ -185,213 +153,11 @@ function SearchPagination({
           {renderCards()}
         </Grid>
       </Grid>
-      { data && data.length > 0 &&
-      <div className={classes.paginationContainer}>
-        <div className={classes.perPageContainer}>
-          Results per Page:
-          <div id="pageSizeBlock" className={classes.pageSizeContainer} onClick={() => setPageListVisible(!pageListVisible)}>
-            {pageSize}
-            <span id="pageSizeArrow" className={pageListVisible? classes.pageSizeArrowUp : classes.pageSizeArrowDown}></span>
-          </div>
-          <div ref={perPageSelection} id="pagelist" className={classes.pageSizeList} style={pageListVisible ? null : {visibility: "hidden"}}>
-            {
-              sizelist.map((sizeItem, idx) => {
-                const key = `size_${idx}`;
-                return (
-                  sizeItem !== pageSize && <div key={key} className={classes.pageSizeItem} onClick={onPageSizeClick}>{sizeItem}</div>
-                )
-              })
-            }
-          </div>
-          <div className={classes.showingContainer}>
-            Showing&nbsp;
-            <div className={classes.showingRangeContainer}>
-              {pageSize*(page-1)+1}
-              -
-              {pageSize*page < count ? pageSize*page : count}&nbsp;
-            </div>
-            of&nbsp;
-            {count}
-          </div>
-        </div>
-        <div className={classes.pageContainer}>
-          <div className={ page === 1 ? classes.prevButtonDisabledContainer : classes.prevButtonContainer} onClick={onPrevious}><div className={ page === 1 ? classes.prevButtonDisabled : classes.prevButton } /></div>
-          <Pagination
-            disabletouchripple="true"
-            classes={{ ul: classes.paginationUl }}
-            className={classes.paginationRoot}
-            count={Math.ceil(count / pageSize)}
-            page={page}
-            siblingCount={2}
-            boundaryCount={1}
-            shape="rounded"
-            hideNextButton
-            hidePrevButton
-            onChange={handleChangePage}
-          />
-          <div className={page === Math.ceil(count / pageSize) ? classes.nextButtonDisabledContainer : classes.nextButtonContainer} onClick={onNext}><div className={ page === Math.ceil(count / pageSize) ? classes.nextButtonDisabled : classes.nextButton} /></div>
-        </div>
-      </div>
-      }
     </>
   );
 }
 
 const styles = {
-  prevButtonContainer: {
-    marginLeft: '10px',
-    border: '1px solid #99A1B7',
-    height: '32px',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  prevButtonDisabledContainer: {
-    marginLeft: '10px',
-    border: '1px solid #99A1B7',
-    height: '32px',
-    '&:hover': {
-      cursor: 'default',
-    },
-  },
-  prevButton: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1px solid #045B80',
-    borderLeft: '1px solid #045B80',
-    margin: '13px 9px 0 11px',
-    transform: 'rotate(45deg)',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  prevButtonDisabled: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1px solid #99A1B7',
-    borderLeft: '1px solid #99A1B7',
-    margin: '13px 9px 0 11px',
-    transform: 'rotate(45deg)',
-  },
-  nextButtonContainer: {
-    borderTop: '1px solid #99A1B7',
-    borderRight: '1px solid #99A1B7',
-    borderBottom: '1px solid #99A1B7',
-    height: '32px',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  nextButtonDisabledContainer: {
-    borderTop: '1px solid #99A1B7',
-    borderRight: '1px solid #99A1B7',
-    borderBottom: '1px solid #99A1B7',
-    height: '32px',
-    '&:hover': {
-      cursor: 'default',
-    },
-  },
-  nextButton: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1px solid #045B80',
-      borderLeft: '1px solid #045B80',
-    margin: '13px 11px 0 9px',
-    transform: 'rotate(225deg)',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  nextButtonDisabled: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1px solid #99A1B7',
-    borderLeft: '1px solid #99A1B7',
-    margin: '13px 11px 0 9px',
-    transform: 'rotate(225deg)',
-  },
-  paginationContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '0 auto',
-    paddingBottom: '30px',
-    '& > *': {
-      marginTop: '10px',
-    },
-  },
-  perPageContainer: {
-    display: 'flex',
-    fontFamily: 'Poppins',
-    fontWeight: '300',
-    fontSize: '14px',
-    color: '#045B80',
-    marginTop: '15px',
-  },
-  pageSizeContainer: {
-    marginLeft: '10px',
-    userSelect: 'none',
-    height: '20px',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  pageSizeArrowUp: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1.5px solid #045B80',
-    borderLeft: '1.5px solid #045B80',
-    margin: '1px 3px 1px 10px',
-    transform: 'rotate(135deg)',
-  },
-  pageSizeArrowDown: {
-    content: "",
-    display: 'inline-block',
-    width: '6px',
-    height: '6px',
-    borderBottom: '1.5px solid #045B80',
-    borderLeft: '1.5px solid #045B80',
-    margin: '1px 3px 3px 10px',
-    transform: 'rotate(-45deg)',
-  },
-  pageSizeList: {
-    position: 'relative',
-    top: '25px',
-    left: '-40px',
-    width: '45px',
-    background: '#F5F5F5',
-    border: '1px solid #99A1B7',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  pageSizeListHidden: {
-    position: 'relative',
-    top: '25px',
-    left: '-30px',
-    width: '45px',
-    border: '1px solid #99A1B7',
-    visibility: 'hidden',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-  pageSizeItem: {
-    padding: '2px 8px',
-    '&:hover': {
-      cursor: 'pointer',
-      color: '#000000',
-    },
-  },
   ul: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -399,53 +165,6 @@ const styles = {
     padding: 0,
     margin: 0,
     listStyle: 'none',
-  },
-  paginationUl: {
-    padding: '2px',
-    '& .MuiPaginationItem-root': {
-      color: '#045B80',
-      fontFamily: 'Poppins',
-      fontSize: '14px',
-      fontWeight: '300',
-      minWidth: '25px',
-      margin: '0',
-      padding: '0 7px',
-    },
-    '& .MuiPaginationItem-page': {
-      transition: 'none',
-    },
-  },
-  paginationRoot: {
-    '& .Mui-selected': {
-      backgroundColor: 'transparent',
-      fontWeight: '600',
-    },
-    '& .Mui-selected:hover': {
-      backgroundColor: 'transparent',
-    },
-    '& .MuiPagination-ul': {
-      padding: '0',
-    },
-    '& .MuiPagination-ul:hover': {
-      cursor: 'pointer',
-    },
-    '& .MuiPagination-ul > li': {
-      height: '32px;',
-      borderTop: '1px solid #99A1B7',
-      borderRight: '1px solid #99A1B7',
-      borderBottom: '1px solid #99A1B7',
-      '&:hover': {
-        backgroundColor: 'transparent',
-      },
-    },
-    '& .MuiPaginationItem-page': {
-      '&:hover': {
-        backgroundColor: 'transparent',
-      },
-    }
-  },
-  content: {
-    fontSize: '12px',
   },
   subsectionBody: {
     maxWidth: '1070px',
@@ -458,16 +177,6 @@ const styles = {
   subsection: {
     '&:last-child $subsectionBody': {
       borderBottom: 'none',
-    },
-  },
-  link: {
-    color: '#DD401C',
-    textDecoration: 'none',
-    '&:hover': {
-      textDecoration: 'underline',
-    },
-    '&:visited': {
-      color: '#9F3D26',
     },
   },
   totalResults: {
@@ -504,25 +213,6 @@ const styles = {
     fontSize: '20px',
     marginBottom: '100px',
   },
-  pageNumber: {
-    margin: '0 5px',
-  },
-  showingContainer: {
-    display: 'flex',
-    position: 'relative',
-    left: '-14px',
-  },
-  showingRangeContainer: {
-    minWidth: '40px',
-    textAlign: 'center',
-  },
-  pageContainer: {
-    display: 'flex',
-    height: '32px',
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  }
 };
 
 export default withStyles(styles)(SearchPagination);

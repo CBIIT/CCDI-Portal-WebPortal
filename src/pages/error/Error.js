a95b22916a2f9f305d01fb2abaf64c4a3158f1e1
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2024-02-09 13:07:44 -0500

    peer review test commit (#304)

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -84,6 +84,7 @@ const Error = () => {
         <div className='titleFirst'>{errorData.titleFirst}</div>
         <div className='titleSecond'>
           <div>{errorData.titleSecond}</div>
+          <div>peer review</div>
         </div>
         <div className='returnButtonContainer'>
            <NavLink className='returnButton' to='/'>{errorData.buttonTitle}</NavLink>

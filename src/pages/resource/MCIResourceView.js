dd5b71a7ced97a1ed04d0fc3025d22a25c539a74
Ben Chen <benchen4998@gmail.com>
2024-03-05 12:27:34 -0500

    PORTAL-1122: Remove List of Institutions Table (2.2.0) (#319)
    * PORTAL-1122: Remove List of Institution Table
    
    * PORTAL-1122: Remove List of Institution Table
    
    * Fix package.json file
    
    * PORTAL-1122: Remove List of Institution Table
    
    * PORTAL-1122: Only remove institution table
    
    * Comment out institution table
    
    * Comment out MCI Institution Table and import

--- src/pages/resource/MCIResourceView.js
+++ src/pages/resource/MCIResourceView.js
@@ -9,7 +9,7 @@ import exportIconBlue from '../../assets/icons/Export_Icon.svg';
 import ccdiDataEcosystemImg from '../../assets/resources/MCI_CCDI_Data_Ecosystem.png';
 import MCITable from './components/MCITable';
 import MCISearchTable from './components/MCISearchTable';
-import MCINumberTable from './components/MCINumberTable';
+//import MCINumberTable from './components/MCINumberTable';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import DonutChart from '../../components/common/DonutChart';
 import MapView from '../../components/common/mapGenerator';
@@ -338,12 +338,12 @@ const MCIResourceView = () => {
                                             </>
                                             }
                                             {mciItem.searchTable && <MCISearchTable table={mciItem.searchTable} /> }
-                                            {mciItem.numberTable && 
+                                            {/*mciItem.numberTable && 
                                             <>
                                                 <MCINumberTable table={mciItem.numberTable} />
                                                 <div>{mciItem.numberTable.footer}</div>
                                             </>
-                                            }
+                                            */}
                                         </div>
                                     </div>
                                 )

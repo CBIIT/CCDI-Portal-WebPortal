ce118c4844311819af69bd27259afcc3026dd036
Ben Chen <benchen4998@gmail.com>
2024-03-07 11:50:50 -0500

    PORTAL-1060: March MCI Enrollment Update (#320)

--- src/bento/mciData.js
+++ src/bento/mciData.js
@@ -26,7 +26,7 @@ export const MCIContent = [
         },
         donut: {
             title: 'Enrollment in MCI (APEC14B1-MCI) by Diagnosis Type',
-            data: [{name: 'Central Nervous System', value: 2085}, {name: 'Rare Tumors', value: 251}, {name: 'Soft Tissue Sarcoma', value: 623}],
+            data: [{name: 'Central Nervous System', value: 2346}, {name: 'Rare Tumors', value: 290}, {name: 'Soft Tissue Sarcoma', value: 70}],
         }
     },
     {

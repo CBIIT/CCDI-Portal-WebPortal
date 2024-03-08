caca9d9c9dfa4b9a7110f6ca2fabfb34677ae051
Ben Chen <benchen4998@gmail.com>
2024-03-08 14:21:42 -0500

    Update src/components/ResponsiveHeader/HeaderDesktop.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/ResponsiveHeader/HeaderDesktop.js
+++ src/components/ResponsiveHeader/HeaderDesktop.js
@@ -94,7 +94,7 @@ const Header = () => {
 
   return (
     <>
-    <USGovBanner>
+      <USGovBanner>
         <div className="USGovBannerInner">
           <div className="bannerLeft">
             <img src={USGovBannerData.logo} alt={"US Flag logo"}></img>

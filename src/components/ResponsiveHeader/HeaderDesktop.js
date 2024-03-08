3d3d2fd1bba1b380b21b792ea1bf7211b533324a
Ben Chen <benchen4998@gmail.com>
2024-03-08 14:21:13 -0500

    Update src/components/ResponsiveHeader/HeaderDesktop.js
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/components/ResponsiveHeader/HeaderDesktop.js
+++ src/components/ResponsiveHeader/HeaderDesktop.js
@@ -70,7 +70,7 @@ const USGovBanner = styled.div`
       display: inline-block;
       background-color: #3b7f84;
       width: 72px;
-        height: 30px;
+      height: 30px;
       border-radius: 5px;
       font-family: "Open Sans", sans-serif;
       font-size: 15px;

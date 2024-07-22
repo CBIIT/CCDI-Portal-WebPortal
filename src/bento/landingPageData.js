0dd29e72dca3a409c96f8048f82f863e8e1fd259
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2024-07-22 14:12:59 -0400

    PORTAL-1377: Update MCI counts text in CCDI Stats at a Glance summary (#390)
    * Added link to stats
    
    * Added icon to stats note
    
    * Added icon after statNum
    
    * updated title Molecular Characterization Initiative

--- src/bento/landingPageData.js
+++ src/bento/landingPageData.js
@@ -45,23 +45,29 @@ export const statsData = [
     num: '',
     title: 'Cataloged Datasets',
     detail: 'Childhood Cancer Data Catalog',
+    link: 'https://datacatalog.ccdi.cancer.gov',
   },
   {
     num: '',
-    title: 'Participants',
+    title: 'Participants with Available Genomic and Clinical Data',
     detail: 'Molecular Characterization Initiative',
+    link: '/MCI',
   },
   {
     num: 58867,
     title: 'Potential Pediatric Molecular Targets',
     detail: 'Molecular Targets Platform',
+    link: 'https://moleculartargets.ccdi.cancer.gov',
   },
   {
     num: 1700440,
     title: 'Reported Cases Under Age 40<br>(1995-2020)',
     detail: 'National Childhood Cancer Registry Explorer',
+    link: 'https://nccrexplorer.ccdi.cancer.gov',
   },
 ];
+
+export const statsNote = 'Counts for MCI participants in data in CCDI and total MCI participants consented may differ.';
 
 export const resourcesAppliationsListData = [
   {

3240c1e2be10f526093f2c01f704db4e591339d4
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2024-11-20 16:30:21 -0500

    Updated cart user guide button link to pdf

--- src/pages/cart/customComponent/userGuideButton/linkButton.js
+++ src/pages/cart/customComponent/userGuideButton/linkButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button, withStyles } from '@material-ui/core';
 import { linkStyles } from './linkStyles';
+import userGuidePDF from '../../../../assets/about/CCDI_Usage_Instructions_Nov2024_v2.5.0.pdf';
 
 const linkButtonView = (props) => {
     const { classes } = props;
     return (
-        <a class="link" href="https://nih.sharepoint.com/:w:/r/sites/NCI-CBIIT-FNLCCDI/_layouts/15/Doc.aspx?sourcedoc=%7B5D16D4D0-5C0F-446A-9AA5-5E2AD325DA55%7D&file=CCDI_Usage_Instructions_October2024_restructured.docx&action=default&mobileredirect=true" target="_blank" rel="noopener noreferrer">
+        <a class="link" href={userGuidePDF} target="_blank" rel="noopener noreferrer">
             <Button className={classes.linkBtn}>User Guide</Button>
         </a>
     );

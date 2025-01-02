261e87771fa802330594bee979c356579414b3f1
Ben Chen <benchen4998@gmail.com>
2025-01-02 10:35:40 -0500

    PORTAL-1805: Add Commas: Explore tab headers+tables pagination

--- src/bento/fileCentricCartWorkflowData.js
+++ src/bento/fileCentricCartWorkflowData.js
@@ -18,7 +18,7 @@ export const navBarCartData = {
 };
 
 // --------------- Files limit configuration --------------
-export const alertMessage = 'The cart is limited to 200000 files. Please narrow the search criteria or remove some files from the cart to add more.';
+export const alertMessage = 'The cart is limited to 200,000 files. Please narrow the search criteria or remove some files from the cart to add more.';
 export const maximumNumberOfFilesAllowedInTheCart = 200000;
 
 // --------------- Tooltip configuration --------------

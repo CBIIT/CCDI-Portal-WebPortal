f0de0b40e23e6a3e2117a3d5fe753739fddc1217
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2025-02-10 12:59:50 -0500

    PORTAL-1846: update user guide (#567)

--- src/pages/cart/customComponent/userGuideButton/linkButton.js
+++ src/pages/cart/customComponent/userGuideButton/linkButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, withStyles } from '@material-ui/core';
 import { linkStyles } from './linkStyles';
-import userGuidePDF from '../../../../assets/about/CCDI_Usage_Instructions_Jan2025_2.5.1_Final.pdf';
+import userGuidePDF from '../../../../assets/about/CCDI_Usage_Instructions_Feb2025_2.5.1_Final.pdf';
 
 const linkButtonView = (props) => {
     const { classes } = props;

bf67316f4bc16c4b6e753c4fae3139f4a12b2301
David-YuWei <ywwei85@gmail.com>
2025-04-17 08:51:26 -0400

    New explore page UI 2

--- src/pages/inventory/inventoryStyle.js
+++ src/pages/inventory/inventoryStyle.js
@@ -17,37 +17,67 @@ export default () => ({
     borderLeft: 'thin solid #8A7F7C',
   },
   sideBar: {
-    width: '270px',
+    width: '180px',
     maxHeight: '1361px',
     overflowX: 'hidden',
-    backgroundColor: 'transparent',
+    backgroundColor: '#007694',
     borderBottom: 'thin solid #8A7F7C',
     overflow: 'auto',
     zIndex: '99',
-    '&::-webkit-scrollbar': {
-      width: '6px',
-      borderWidth: '0px 1px 1px 1px',
-      borderStyle: 'solid',
-      borderColor: '#B0B0B0',
-    },
-    '&::-webkit-scrollbar-track': {
-      backgroundColor: '#CECECE',
-    },
-    '&::-webkit-scrollbar-thumb': {
-      backgroundColor: '#003F74',
-    },
+    color: '#ffffff',
   },
   sideBarMenuSider: {
-
+    width: '180px',
   },
   siderContent: {
     listStyle: 'none',
+    padding: '0px',
+    margin: '0px',
+    '& li': {
+      height: '50px',
+      lineHeight: '50px',
+      padding: '0 13px',
+    }
   },
   facetCategory: {
-
+    cursor: 'pointer',
+    '&:hover': {
+      opacity: '0.8',
+    }
   },
+  facetCategoryActive: {
+    cursor: 'pointer',
+    backgroundColor: '#00546e',
+  }, 
+  categoryIcon: {
+    lineHeight: '22px',
+    fontSize: '22px',
+    verticalAlign: '-5px',
+  },
+  categoryTitle: {
+    paddingLeft: '15px',
+  },
+  sidebarMenuContentPanel: {
+    width: '270px',
+    padding: '16px 16px 0 16px',
+  },
+  contentPanelHeader: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    fontSize: '14px',
+    height: '22px',
+    cursor: 'pointer',
+    '& a': {
+      width: '16px',
+      '& svg': {
+        width: '16px',
+        height: '16px',
+      }
+    }
+  },
+  contentPanelBody: {},
   rightContent: {
-    width: 'calc(100% - 270px)',
+    width: 'calc(100% - 450px)',
     position: 'relative',
     borderRight: 'thin solid #8A7F7C',
     borderLeft: 'thin solid #8A7F7C',

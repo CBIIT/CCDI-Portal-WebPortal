fe223cc11865121546839626d2be156ba859dbe8
Ben Chen <benchen4998@gmail.com>
2025-04-28 12:00:01 -0400

    fix query bar generator

--- src/pages/inventory/filterQueryBar/QueryBarView.js
+++ src/pages/inventory/filterQueryBar/QueryBarView.js
@@ -45,7 +45,7 @@ const QueryBarView = ({ data, statusReducer, localFind }) => {
           'treatment_type': '', 'treatment_agent': '', 'age_at_treatment_start': '', 'response_category': '', 'age_at_response': '', 
           'age_at_last_known_survival_status': '', 'first_event': '', 'last_known_survival_status': '', 
           'participant_age_at_collection': '', 'sample_anatomic_site': '', 'sample_tumor_status': '', 'tumor_classification': '', 
-          'data_category': '', 'file_type': '', 'dbgap_accession': '', 'study_name': '', 'study_status': '',
+          'data_category': '', 'file_type': '', 'file_mapping_level': '', 'dbgap_accession': '', 'study_name': '', 'study_status': '',
           'library_selection': '', 'library_strategy': '', 'library_source_material': '', 'library_source_molecule': ''
         };
         const queryStr = generateQueryStr(query, queryParams, paramValue);

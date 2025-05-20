a41514ce80b870746bb89b8f5b5752845199fd42
Francisco M. Miranda <fmanuelos@gmail.com>
2025-05-20 11:52:43 -0400

    Update DataModelNavigator iframe sandbox

--- src/pages/dmn/DataModelNavigator.js
+++ src/pages/dmn/DataModelNavigator.js
@@ -11,7 +11,7 @@ const DataModelNavigator = () => {
         scrolling="no"
         title="Data Model Navigator"
         style={{ width: '100%', height: '1000px', border: 'none' }}
-        sandbox="allow-popups allow-scripts allow-same-origin"
+        sandbox="allow-popups allow-scripts allow-same-origin allow-downloads"
       />
     </div>
   );

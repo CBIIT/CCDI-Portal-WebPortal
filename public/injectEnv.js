93dfa0320cef3ee5be0ee2ed4a5cb78def891a0b
Ben Chen <benchen4998@gmail.com>
2025-05-22 09:48:21 -0400

    revert inject to default

--- public/injectEnv.js
+++ public/injectEnv.js
@@ -2,7 +2,7 @@ window.injectedEnv = {
   REACT_APP_BACKEND_GETUSERINFO_API: 'https://bento-tools.org/v1/graphql/',
   REACT_APP_LOGIN_URL: 'https://bento-tools.org/v1/graphql/',
   REACT_APP_USER_LOGOUT_URL: 'https://bento-tools.org/v1/graphql/',
-  REACT_APP_BACKEND_API: 'https://ccdi-dev.cancer.gov/v1/graphql/',
+  REACT_APP_BACKEND_API: 'http://localhost:8080/v1/graphql/',
   REACT_APP_AUTH_API: 'https://bento-tools.org/v1/graphql/',
   REACT_APP_GOOGLE_CLIENT_ID: 'REACT_APP_GOOGLE_CLIENT_ID',
   REACT_APP_APPLICATION_VERSION: 'YYYY_MM_DD/HH:MM',

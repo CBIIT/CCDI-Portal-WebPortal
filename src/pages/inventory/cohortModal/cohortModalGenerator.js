701ea3b7f9ed20a83b8ca1c255713f6afe9e1aa9
Francisco M. Miranda <fmanuelos@gmail.com>
2025-06-24 17:37:01 -0400

    Cohort-The confirmation message is not displayed when user makes the unsaved changes to the already created cohort

--- src/pages/inventory/cohortModal/cohortModalGenerator.js
+++ src/pages/inventory/cohortModal/cohortModalGenerator.js
@@ -12,10 +12,13 @@ import DEFAULT_STYLES from './styles';
 import DEFAULT_CONFIG from './config';
 import CohortList from './components/cohortList';
 import CohortDetails from './components/cohortDetails';
+import DeleteConfirmationModal from './components/deleteConfirmationModal';
+import { deletionTypes } from './components/deleteConfirmationModal';
 import Alert from '@material-ui/lab/Alert';
 import { GET_COHORT_MANIFEST_QUERY, GET_COHORT_METADATA_QUERY } from '../../../bento/dashboardTabData.js';
 import client from '../../../utils/graphqlClient.js'
-import { arrayToCSVDownload, objectToJsonDownload } from './utils.js';
+import { arrayToCSVDownload, objectToJsonDownload, hasUnsavedChanges} from './utils.js';
+import { CohortModalContext } from './CohortModalContext.js'
 
 /**
  * Generator function to create cohortModal component with custom configuration
@@ -29,9 +32,21 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
         config, functions,
     } = uiConfig;
 
+
+     const { currentCohortChanges, setCurrentCohortChanges } = useContext(CohortModalContext);
+
     const { state, dispatch } = useContext(CohortStateContext);
     const [selectedCohort, setSelectedCohort] = useState(null); // Default to the first entry
     const [alert, setAlert] = useState({ type: '', message: '' });
+
+    const unSavedChanges = currentCohortChanges ? hasUnsavedChanges(currentCohortChanges, state[selectedCohort]) : false;
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+    const [deleteModalProps, setDeleteModalProps] = useState({
+        handleDelete: () => { },
+        deletionType: "",
+    });
+
+
     useEffect(() => {
         if (alert.message) {
             const timer = setTimeout(() => {
@@ -80,6 +95,23 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
     const handleDeleteAllCohorts = () => {
         dispatch(onDeleteAllCohort());
     };
+
+    // Handle saving updates to cohort
+    const handleSetCurrentCohortChanges = (localCohort) => {
+        if (!localCohort.cohortId) return;
+        setCurrentCohortChanges({
+            cohortId: localCohort.cohortId,
+            cohortName: localCohort.cohortName,
+            cohortDescription: localCohort.cohortDescription,
+            participants: localCohort.participants,
+            searchText: localCohort.searchText,
+        })
+    };
+
+    const handleClearCurrentCohortChanges = () => {
+        setCurrentCohortChanges(null);
+    };
+
     // Handle saving updates to cohort
     const handleSaveCohort = (localCohort) => {
         if (!localCohort.cohortId) return;
@@ -90,7 +122,10 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
                 cohortDescription: localCohort.cohortDescription,
                 participants: localCohort.participants
             },
-            () => setAlert({ type: 'success', message: 'Cohort updated successfully!' }),
+            () => {
+                setAlert({ type: 'success', message: 'Cohort updated successfully!' })
+                handleClearCurrentCohortChanges();
+            },
             (error) => setAlert({ type: 'error', message: `Failed to update cohort: ${error.message}` })
         ));
     };
@@ -115,17 +150,31 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
                 setSelectedCohort(null);
             };
 
+            const unSavedChangesCheck = () => {
+                if (unSavedChanges) {
+                    setDeleteModalProps({
+                        handleDelete: () => closeModalWrapper(),
+                        deletionType: deletionTypes.CLEAR_UNSAVED_CHANGES,
+                    });
+                    setShowDeleteConfirmation(true)
+                }
+                else {
+                    closeModalWrapper()
+                }
+            }
+
             return (
+                <>
                 <Modal
                     {...props}
                     open={open}
                     className={classes.modal}
-                    onClose={closeModalWrapper}
+                    onClose={unSavedChangesCheck}
                 >
                     <div className={classes.paper}>
                         <h1 className={classes.modalTitle}>
                             <span>{modalTitle}</span>
-                            <span className={classes.closeIcon} onClick={closeModalWrapper}>
+                            <span className={classes.closeIcon} onClick={unSavedChangesCheck}>
                                 <img
                                     src="https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/svgs/LocalFindCaseDeleteIcon.svg"
                                     alt="close icon"
@@ -140,13 +189,18 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
                         </h1>
                         <div className={classes.modalContainer}>
                             <CohortList
+                
                                 classes={cohortListClasses}
                                 config={config.cohortList}
                                 selectedCohort={selectedCohort}
                                 setSelectedCohort={setSelectedCohort}
-                                closeParentModal={closeModalWrapper}
+                                unSavedChanges={unSavedChanges}
+                                setChangingConfirmation={setDeleteModalProps}
+                                setShowChangingConfirmation={setShowDeleteConfirmation}
+                                closeParentModal={unSavedChangesCheck}
                                 handleDeleteCohort={handleDeleteCohort}
                                 handleDeleteAllCohorts={handleDeleteAllCohorts}
+                                handleClearCurrentCohortChanges={handleClearCurrentCohortChanges}
                                 deleteConfirmationClasses={deleteConfirmationClasses}
                                 state={state}
                             />
@@ -154,8 +208,10 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
                                 classes={cohortDetailsClasses}
                                 config={config.cohortDetails}
                                 activeCohort={state[selectedCohort]}
-                                closeModal={closeModalWrapper}
+                                temporaryCohort={currentCohortChanges}
+                                closeModal={unSavedChangesCheck}
                                 handleSaveCohort={handleSaveCohort}
+                                handleSetCurrentCohortChanges={handleSetCurrentCohortChanges}
                                 downloadCohortManifest={downloadCohortManifest}
                                 downloadCohortMetadata={downloadCohortMetadata}
                                 deleteConfirmationClasses={deleteConfirmationClasses}
@@ -163,6 +219,16 @@ export const CohortModalGenerator = (uiConfig = DEFAULT_CONFIG) => {
                         </div>
                     </div>
                 </Modal>
+                
+                <DeleteConfirmationModal
+                    classes={deleteConfirmationClasses}
+                    open={showDeleteConfirmation}
+                    setOpen={setShowDeleteConfirmation}
+                    handleDelete={deleteModalProps.handleDelete}
+                    deletionType={deleteModalProps.deletionType}
+                />
+
+                </>
             )
         }),
     };
--- src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
+++ src/pages/inventory/cohortModal/components/deleteConfirmationModal.js
@@ -5,6 +5,7 @@ export const deletionTypes = {
     DELETE_ALL_COHORTS: 'ALL cohorts',
     DELETE_SINGLE_COHORT: 'this cohort',
     DELETE_ALL_PARTICIPANTS: 'ALL participants',
+    CLEAR_UNSAVED_CHANGES: 'leave?'
 };
 
 const DeleteConfirmationModal = (props) => {
@@ -13,7 +14,8 @@ const DeleteConfirmationModal = (props) => {
         open,
         setOpen,
         handleDelete,
-        deletionType
+        deletionType,
+        message
     } = props;
 
     return (
@@ -24,25 +26,43 @@ const DeleteConfirmationModal = (props) => {
             <div className={classes.modal}>
                 <div className={classes.modalContent}>
                     <div className={classes.modalHeading}>
-                        <span>Are you sure you want to delete {deletionType}? {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && 'This action cannot be undone.'}</span>
-                        <span>Press Confirm or Cancel.</span>
+                        {message ?
+                            <span>{message}</span>
+                            :
+                            <>
+                                <span> Are you sure you want to {deletionType} </span>
+                                {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>{deletionType === deletionTypes.CLEAR_UNSAVED_CHANGES && 'You will lose all unsaved changes. ' }This action cannot be undone.</span>}
+                                {deletionType !== deletionTypes.DELETE_ALL_PARTICIPANTS && <span>Press Confirm or Cancel. </span>}
+                            </>
+                        }
                     </div>
                     <div className={classes.modalButtons}>
-                        <button
-                            className={classes.cancelButton}
-                            onClick={() => setOpen(false)}
-                        >
-                            Cancel
-                        </button>
-                        <button
-                            className={classes.confirmButton}
-                            onClick={() => {
-                                handleDelete();
-                                setOpen(false);
-                            }}
-                        >
-                            Confirm
-                        </button>
+                        {message ?
+                            <button className={classes.confirmButton}
+                                onClick={() => {
+                                    setOpen(false);
+
+                                }}
+                            >Ok</button>
+                            :
+                            <>
+                                <button
+                                    className={classes.cancelButton}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className={classes.confirmButton}
+                                    onClick={() => {
+                                        handleDelete();
+                                        setOpen(false);
+                                    }}
+                                >
+                                    Confirm
+                                </button>
+                            </>
+                        }
                     </div>
                 </div>
             </div>

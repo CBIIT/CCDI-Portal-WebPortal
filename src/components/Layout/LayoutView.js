dbbd7b63463582d269dacf4304043cb28f77cbfd
Ruoxi Zhang <116577061+RuoxiZhang08@users.noreply.github.com>
2025-08-29 10:24:56 -0400

    PORTAL-1983: New resource page: Pediatric, Adolescent, and Young Adult Rare Cancer Study (#721)
    * Pediatric, Adolescent, and Young Adult Rare Cancer Study
    
    * Adjust resourceTitleText length

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -24,6 +24,7 @@ import StudiesDetail from "../../pages/studyDetail/studyDetailController";
 import OverlayWindow from '../OverlayWindow/OverlayWindow';
 import CohortAnalyzerController  from "../../pages/CohortAnalyzer/CohortAnalyzerController";
 import ToolsResourceView from "../../pages/resource/ToolsResourcePage/ToolsResourceController";
+import RareCancerResourceView from "../../pages/resource/RareCancerResourcePage/RareCancerResourceController";
 // import NewsDetail from '../../pages/news/newsDetailView';
 
 const Layout = () => {
@@ -47,6 +48,7 @@ const Layout = () => {
           <Route path="/ccdi-participant-index" element={<CPIResourceView />} />
           <Route path="/publications" element={<PublicationsView />} />
           <Route path="/tools" element={<ToolsResourceView />} />
+          <Route path="/pediatric-adolescent-and-young-adult-rare-cancer-study" element={<RareCancerResourceView/>} />
           <Route path="/release-notes" element={<ReleaseNotesPageView />} />
           <Route path="/cohortAnalyzer" element={<CohortAnalyzerController />} />
           <Route path="/studies" >

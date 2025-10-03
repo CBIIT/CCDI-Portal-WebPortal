96d862ea0eec1e1ef47edb36259be013f81e805a
Francisco M. Miranda <francisco.miranda@nih.gov>
2025-10-03 11:25:52 -0400

    Update FederationDMN URL construction logic
    Removed REACT_APP_FED_DMN_URL from injected environment variables and refactored FederationDMN.js to dynamically construct the federation DMN URL based on REACT_APP_DMN_URL. This improves maintainability and ensures consistent URL formatting.

--- src/pages/resource/FederationDMN/FederationDMN.js
+++ src/pages/resource/FederationDMN/FederationDMN.js
@@ -3,7 +3,10 @@ import React from 'react';
 import env from '../../../utils/env';
 
 const FederationDataModelNavigator = () => {
-  const fedDmnUrl = env.REACT_APP_FED_DMN_URL  || "https://cbiit.github.io/crdc-data-model-navigator/?config=https://raw.githubusercontent.com/CBIIT/ccdi-federation-dmn/refs/heads/main/";
+  const dmnUrl = (env.REACT_APP_DMN_URL || "https://cbiit.github.io/crdc-data-model-navigator/?config=https://raw.githubusercontent.com/CBIIT/ccdi-model/refs/heads/dmn-dev/model-desc/").split('?config=')[0] + '?config=';
+
+  const fedDmnUrl = dmnUrl + 'https://raw.githubusercontent.com/CBIIT/ccdi-federation-dmn/refs/heads/main/';
+  
   return (
     <div style={{ width: '100%', height: '1000px' }}>
       <iframe

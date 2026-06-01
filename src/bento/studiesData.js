d931c55ada30a9e34b8fa637e97460f74dbbdb34
David-YuWei <ywwei85@gmail.com>
2026-06-01 18:00:45 -0400

    Add metadata files link for new study phs004222

--- src/bento/studiesData.js
+++ src/bento/studiesData.js
@@ -45,6 +45,7 @@ const studyDownloadLinks = {
   "phs003432": "https://d2xnga7irezzit.cloudfront.net/metadata_files/phs003432_CCDI_Study_Manifest_v3.1.0.xlsx",
   "phs003519": "https://d2xnga7irezzit.cloudfront.net/metadata_files/phs003519_CCDI_Study_Manifest_v3.1.0.xlsx",
   "phs003975": "https://d2xnga7irezzit.cloudfront.net/metadata_files/phs003975_CCDI_Study_Manifest_v3.1.0.xlsx",
+  "phs004222": "https://d2xnga7irezzit.cloudfront.net/metadata_files/phs004222_CCDI_Study_Manifest_v3.1.0.xlsx",
 };
 
 const studycBioPortalLinks = {

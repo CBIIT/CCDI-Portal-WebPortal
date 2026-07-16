1573bc0b56deafe3659e01fb27648e561bf8a58a
Yizhen Chen <cheny39@nih.gov>
2026-07-16 13:39:29 -0400

    update the warning bar behavior

--- src/components/OverlayWindow/OverlayWindow.js
+++ src/components/OverlayWindow/OverlayWindow.js
@@ -13,66 +13,57 @@ import {
   DialogActions,
 } from '@material-ui/core';
 import FiberManualRecord from '@material-ui/icons/FiberManualRecord';
-// import { useSelector } from 'react-redux';
 import * as text from './OverlayText.json';
-import DialogThemeProvider from './OverlayThemConfig';
-import { setOverLayWindow } from '../../pages/globalSearch/store/sitesearchReducer';
+import DialogThemeProvider from './OverlayThemeConfig';
 
 const OverlayWindow = () => {
   const [open, setOpen] = useState(false);
 
   const handleClose = () => {
     setOpen(false);
-    setOverLayWindow(false);
-    localStorage.setItem('overlayLoad', 'true');
+    sessionStorage.setItem('overlayLoad', 'true');
   };
 
-  useEffect(() => { 
-    if (!localStorage.getItem('overlayLoad')) {
+  useEffect(() => {
+    if (!sessionStorage.length) {
       setOpen(true);
-      setOverLayWindow(true);
     }
-  }, [open]);
+  }, []);
 
-  const content = text.content.map((item, index) => (
-    <DialogContentText key={`${index}`} id="alert-dialog-description">{item}</DialogContentText>
+  const content = text.content.map((item) => (
+    <DialogContentText key={item.substring(0, 30)}>{item}</DialogContentText>
   ));
-  const list = text.list.map((item, index) => (
-    <ListItem key={`${index}`}>
+  const list = text.list.map((item) => (
+    <ListItem key={item.substring(0, 30)}>
       <ListItemIcon>
         <FiberManualRecord style={{ fontSize: 8 }} />
       </ListItemIcon>
-      <ListItemText>{item}</ListItemText>
+      <ListItemText primary={item} />
     </ListItem>
   ));
 
   return (
-    <>
-      <div>
-        <DialogThemeProvider>
-          <Dialog
-            open={open}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            maxWidth="md"
-          >
-            <DialogTitle id="alert-dialog-title">{'Warning'}</DialogTitle>
-            <Divider />
-            <DialogContent>
-              {content}
-              {' By using this system, you understand and consent to the following: '}
-              <List>
-                {list}
-              </List>
-            </DialogContent>
-            <Divider />
-            <DialogActions>
-              <Button onClick={handleClose}>{'Continue'}</Button>
-            </DialogActions>
-          </Dialog>
-        </DialogThemeProvider>
-      </div>
-    </>
+    <DialogThemeProvider>
+      <Dialog
+        open={open}
+        aria-labelledby="alert-dialog-title"
+        maxWidth="md"
+      >
+        <DialogTitle id="alert-dialog-title">Warning</DialogTitle>
+        <Divider />
+        <DialogContent>
+          {content}
+          {' By using this system, you understand and consent to the following: '}
+          <List>
+            {list}
+          </List>
+        </DialogContent>
+        <Divider />
+        <DialogActions>
+          <Button onClick={handleClose}>Continue</Button>
+        </DialogActions>
+      </Dialog>
+    </DialogThemeProvider>
   );
 };
 

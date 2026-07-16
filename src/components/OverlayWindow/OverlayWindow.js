1dda5a93b2ba2d6a6a5738f9ea991b2834ac3d8c
Yizhen Chen <cheny39@nih.gov>
2026-07-16 13:26:38 -0400

    add missing session js

--- src/components/OverlayWindow/OverlayWindow.js
+++ src/components/OverlayWindow/OverlayWindow.js
@@ -25,7 +25,7 @@ const OverlayWindow = () => {
   };
 
   useEffect(() => {
-    if (sessionStorage.getItem('overlayLoad') !== 'true') {
+    if (!sessionStorage.length) {
       setOpen(true);
     }
   }, []);
